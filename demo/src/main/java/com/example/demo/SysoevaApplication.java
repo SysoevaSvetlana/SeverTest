f8b6b045c98e3c05532a80d09d9d3e93344728e9
SvetlanaSysoeva03 <dorri.ribka@yandex.ru>
2026-02-04 14:09:09 +0300

    initional commit

---
+++ demo/src/main/java/com/example/demo/SysoevaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysoevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysoevaApplication.class, args);
+	}
+
+}
